f768b2e45387a0a2332c39e602e67ac3e90d2593
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-11-11 16:57:07 +0000

    Add #ordered_page_visits to Parser
    This makes use of the various classes in lib to process a given logfile and
    output a list of pages ordered by visits.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,7 +1,19 @@
+require_relative "log_validator"
+require_relative "page_visit_counter"
+require_relative "visit_count_orderer"
+require_relative "visits_printer"
+
 class Parser
   attr_reader :logfile
 
   def initialize(logfile)
     @logfile = logfile
+  end
+
+  def ordered_page_visits
+    validated_pages = LogValidator.new(logfile).validate
+    page_visit_count = PageVisitCounter.new(validated_pages).count_visits
+    ordered_page_visits = VisitCountOrderer.new(page_visit_count).order_descending
+    VisitsPrinter.new(ordered_page_visits).print_visits
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,11 +3,21 @@ require "parser"
 
 RSpec.describe Parser do
   let(:valid_logfile) { File.open("spec/fixtures/valid_logfile.log") }
+  let(:parser) { described_class.new(valid_logfile) }
 
   describe "#initialize" do
     it "accepts a logfile as an argument" do
-      parser = described_class.new(valid_logfile)
       expect(parser.logfile).to eq(valid_logfile)
+    end
+  end
+
+  describe "#ordered_page_visits" do
+    it "returns a list of page paths ordered by visits in descending order" do
+      page_visits = [["/help_page/1", 5], ["/home", 3], ["/contact", 2], ["/about/2", 1]]
+      output = ""
+      page_visits.each { |item| output += "#{item[0]} #{item[1]} page visits\n" }
+
+      expect { parser.ordered_page_visits }.to output(output).to_stdout
     end
   end
 end
